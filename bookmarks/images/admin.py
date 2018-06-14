535a12646c3d6a3d57e6e2f63261089649111fe2
hanhansoul <whr2290@163.com>
2018-06-15 00:21:09 +0800

    chapter 5 images

---
+++ bookmarks/images/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+
+
+# Register your models here.
+from images.models import Image
+
+
+class ImageAdmin(admin.ModelAdmin):
+    list_display = ['title', 'slug', 'image', 'created']
+    list_filter = ['created']
+
+
+admin.site.register(Image, ImageAdmin)
