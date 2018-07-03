885b782d3c158d00c318945526d539637b598485
hanhansoul <whr2290@163.com>
2018-07-04 00:41:02 +0800

    20180704

--- myshop/myshop/settings.py
+++ myshop/myshop/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'shop.apps.ShopConfig',
+    'cart.apps.CartConfig',
 ]
 
 MIDDLEWARE = [
@@ -117,3 +118,6 @@ STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
+CART_SESSION_ID = 'cart'
+
